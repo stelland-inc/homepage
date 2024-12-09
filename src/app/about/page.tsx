7456cba7423ad972ca6b986d3b213320f9fb92f7
Dami <dami@stelland.io>
2024-12-09 19:47:06 +0900

    added dynamic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,51 +1,21 @@
 'use client'
 import Image from 'next/image'
 import styles from './page.module.scss'
-import { useRef, useEffect, useCallback } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+
 import Roadmap from '@/components/Card/History/Roadmap';
 import { useLanguage } from '@/contexts/LanguageContext';
 import Slider from '@/components/Slider';
 import Footer from '@/components/Footer';
+// import SlidingText from '@/components/SlidingText/SlidingText';
+import dynamic from 'next/dynamic'
 
-export default function About() {
-  const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  const directionRef = useRef(-1);
+// For components that use browser-only APIs
+const SlidingText = dynamic(() => import('@/components/SlidingText/SlidingText'), {
+  ssr: false
+})
+
+export default function About() { 
   const { language } = useLanguage();
-
-  const xPercentRef = useRef(0); // Using useRef to store xPercent
-
-  // Wrap the animate function in useCallback
-  const animate = useCallback(() => {
-    if (xPercentRef.current < -100) {
-      xPercentRef.current = 0;
-    } else if (xPercentRef.current > 0) {
-      xPercentRef.current = -100;
-    }
-    gsap.set(firstText.current, { xPercent: xPercentRef.current });
-    gsap.set(secondText.current, { xPercent: xPercentRef.current });
-    requestAnimationFrame(animate);
-    xPercentRef.current += 0.1 * directionRef.current;
-  }, []); // Empty dependency array ensures this function is only created once
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-        scrollTrigger: {
-            trigger: document.documentElement,
-            scrub: 0.25,
-            start: 0,
-            end: window.innerHeight,
-            onUpdate: e => directionRef.current = e.direction * -1
-        },
-        x: "-500px",
-    });
-    requestAnimationFrame(animate);
-  }, [animate]);
-
   const roadmapData = [
     {
       title: '레진코믹스 지정 현지화 업체',
@@ -86,32 +56,7 @@ export default function About() {
   ];
 
     return <>
-        <main className={styles.main}>
-            {/* <Image 
-                src="/images/about-bg.jpg"
-                fill={true}
-                alt="background image by Austin Distel, unsplash"
-                className='bg-red-200'
-            /> */}
-            <div className='bg-red-200 w-full h-full'>
-
-                <div className='flex flex-col justify-start items-start h-full max-w-screen-xl mx-auto'>
-                        <div className='text-white text-left text-2xl font-bold mt-[35vh]'>
-                        {language === 'en' ? <p className='md:pl-0 pl-10'>We are your content globalization partner.</p> 
-                                           : <p className='md:pl-0 pl-10'>스텔라앤은 여러분의 콘텐츠 글로벌화 여정을 함께 합니다.</p>}
-                        </div>
-                </div>
-
-            </div>
-            {/* Photo by <a href="https://unsplash.com/@austindistel?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Austin Distel</a> on <a href="https://unsplash.com/photos/person-reading-book-during-daytime-Ej_GTF0JPss?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-       */}
-            <div className={styles.sliderContainer}>
-                <div ref={slider} className={styles.slider}>
-                    <p ref={firstText}>Your Story, Our Craft</p>
-                    <p ref={secondText}>Your Story, Our Craft</p>
-                </div>
-            </div>
-            </main>
+        <SlidingText />
 
         <div className="max-w-screen-xl mx-auto pt-20 md:p-0 p-5 ">
         <div className="flex md:flex-row flex-col justify-center md:items-start items-center md:gap-0 gap-5 md:pt-20 my-24">
---
+++ src/components/SlidingText/SlidingText.tsx
@@ -0,0 +1,67 @@
+import { useRef, useEffect, useCallback } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { useLanguage } from '@/contexts/LanguageContext';
+import styles from './style.module.scss';
+
+const SlidingText = () => {
+    const firstText = useRef(null);
+    const secondText = useRef(null);
+    const slider = useRef(null);
+    const directionRef = useRef(-1);
+    const { language } = useLanguage();
+  
+    const xPercentRef = useRef(0); // Using useRef to store xPercent
+  
+    // Wrap the animate function in useCallback
+    const animate = useCallback(() => {
+      if (xPercentRef.current < -100) {
+        xPercentRef.current = 0;
+      } else if (xPercentRef.current > 0) {
+        xPercentRef.current = -100;
+      }
+      gsap.set(firstText.current, { xPercent: xPercentRef.current });
+      gsap.set(secondText.current, { xPercent: xPercentRef.current });
+      requestAnimationFrame(animate);
+      xPercentRef.current += 0.1 * directionRef.current;
+    }, []); // Empty dependency array ensures this function is only created once
+  
+    useEffect(() => {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.to(slider.current, {
+          scrollTrigger: {
+              trigger: document.documentElement,
+              scrub: 0.25,
+              start: 0,
+              end: window.innerHeight,
+              onUpdate: e => directionRef.current = e.direction * -1
+          },
+          x: "-500px",
+      });
+      requestAnimationFrame(animate);
+    }, [animate]);
+  
+    return (
+        <main className={styles.main}>
+            <div className='bg-red-200 w-full h-full'>
+
+                <div className='flex flex-col justify-start items-start h-full max-w-screen-xl mx-auto'>
+                        <div className='text-white text-left text-2xl font-bold mt-[35vh]'>
+                        {language === 'en' ? <p className='md:pl-0 pl-10'>We are your content globalization partner.</p> 
+                                           : <p className='md:pl-0 pl-10'>스텔라앤은 여러분의 콘텐츠 글로벌화 여정을 함께 합니다.</p>}
+                        </div>
+                </div>
+
+            </div>
+            <div className={styles.sliderContainer}>
+                <div ref={slider} className={styles.slider}>
+                    <p ref={firstText}>Your Story, Our Craft</p>
+                    <p ref={secondText}>Your Story, Our Craft</p>
+                </div>
+            </div>
+            </main>
+
+    )
+}
+
+export default SlidingText;
