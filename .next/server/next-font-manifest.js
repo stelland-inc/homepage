c313cb1945d1645a6ca235f22fa9506a7de9f25b
Jongmin Baek <jongminbaek@stelland.io>
2024-12-09 19:20:47 +0900

    Delete .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/commuglobal/homepage/src/app/layout\":[\"static/media/120a5a1920781bd0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
