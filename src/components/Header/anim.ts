a9ffb811b92bf7f4a9ffbc5cc2d259b6d221106d
Rafi Ramadhan <rafiisakh@stelland.io>
2026-09-15 18:39:39 +0900

    update dependencies

--- src/components/Header/anim.ts
+++ src/components/Header/anim.ts
@@ -1,4 +1,4 @@
-const transition = {duration: 1, ease: [0.76, 0, 0.24, 1]}
+const transition = {duration: 1, ease: [0.76, 0, 0.24, 1] as const}
 
 export const opacity = {
     initial: {
@@ -68,11 +68,11 @@ export const translate = {
     enter: (i) => ({
         y: 0,
         opacity: 1,
-        transition: {duration: 1, ease: [0.76, 0, 0.24, 1], delay: i[0]}
+        transition: {duration: 1, ease: [0.76, 0, 0.24, 1] as const, delay: i[0]}
     }),
     exit: (i) => ({
         y: "100%",
         opacity: 0,
-        transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: i[1]}
+        transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1] as const, delay: i[1]}
     })
 }
